6e1add1dfde435628be7e3137d0abd77dae071fe
Mohammadreza khahani <mr.khahani@gmail.com>
2021-04-19 13:04:14 +0430

    Update AccountTest

--- src/test/java/com/khahani/demo/account/AccountTest.java
+++ src/test/java/com/khahani/demo/account/AccountTest.java
@@ -1,6 +1,5 @@
 package com.khahani.demo.account;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,25 +14,64 @@ class AccountTest {
     }
 
     @Test
-    public void add_one_credit_record(){
-        Record r = new Credit();
-        r.setAmount(1000);
-        account.addRecord(r);
+    public void when_records_are_balanced_then_balance_is_zero(){
+        addRecords("z");
+        assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void when_having_more_credit_then_balance_is_positive(){
+        addRecords("p");
         assertEquals(1000, account.getBalance());
     }
 
     @Test
-    public void add_two_credit_record(){
+    public void when_having_more_debit_then_balance_is_negative(){
+        addRecords("n");
+        assertEquals(-1000, account.getBalance());
+    }
+
+    /**
+     * @param balanceStatus z = zero, p = positive, n = negative
+     */
+    private void addRecords(String balanceStatus) {
         account = new Account();
 
-        Record r = new Credit();
-        r.setAmount(1000);
-        account.addRecord(r);
+        Record d1 = new Debit();
+        d1.setAmount(1000);
+        account.addRecord(d1);
 
-        Record r2 = new Credit();
-        r2.setAmount(1000);
-        account.addRecord(r2);
+        Record d2 = new Debit();
+        d2.setAmount(1000);
+        account.addRecord(d2);
 
-        assertEquals(2000, account.getBalance());
+        Record d3 = new Debit();
+        d3.setAmount(2000);
+        account.addRecord(d3);
+
+        Record c1 = new Credit();
+        c1.setAmount(1000);
+        account.addRecord(c1);
+
+        Record c2 = new Credit();
+        c2.setAmount(1000);
+        account.addRecord(c2);
+
+        Record c3 = new Credit();
+        c3.setAmount(2000);
+        account.addRecord(c3);
+
+        if (balanceStatus.equals("p")){
+            Record c4 = new Credit();
+            c4.setAmount(1000);
+            account.addRecord(c4);
+        }
+
+        if (balanceStatus.equals("n")){
+            Record d4 = new Debit();
+            d4.setAmount(1000);
+            account.addRecord(d4);
+        }
     }
+
 }
