bc779ac35a3b5789b1338c0a99aa4901de8f7bcd
Mohammadreza khahani <mr.khahani@gmail.com>
2021-04-19 11:28:19 +0430

    update Record and add more field

--- src/main/java/com/khahani/demo/account/Record.java
+++ src/main/java/com/khahani/demo/account/Record.java
@@ -2,6 +2,11 @@ package com.khahani.demo.account;
 
 public class Record {
     private int amount;
+    private int day;
+    private int month;
+    private String description;
+    private Status status;
+
 
     public void setAmount(int amount) {
         if (amount <= 0)
@@ -14,10 +19,32 @@ public class Record {
         return amount;
     }
 
-    public static class CantEnterZeroAmountException extends RuntimeException{
+    public static class CantEnterZeroAmountException extends RuntimeException {
         @Override
         public String getMessage() {
             return "Amount is not valid";
+        }
+    }
+
+    enum Status {
+        Debit(-1),
+        Credit(1);
+
+        int value;
+
+        Status(int v) {
+            value = v;
+        }
+
+        int getOperator() {
+            return value;
+        }
+
+        String getText() {
+            if (value == 1)
+                return "بستانکار";
+            else
+                return "بدهکار";
         }
     }
 }
