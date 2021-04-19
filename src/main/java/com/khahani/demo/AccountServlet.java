f64a221726b127a6438c5f1e7fa6e8b7a59b2882
Mohammadreza khahani <mr.khahani@gmail.com>
2021-04-19 14:39:10 +0430

    Bugfix: update AccountServlet to create Record correctly

--- src/main/java/com/khahani/demo/AccountServlet.java
+++ src/main/java/com/khahani/demo/AccountServlet.java
@@ -1,5 +1,7 @@
 package com.khahani.demo;
 
+import com.khahani.demo.account.Record;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -15,5 +17,24 @@ public class AccountServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        Record record = null;
+        int day = Integer.parseInt(request.getParameter("day"));
+        int month = Integer.parseInt(request.getParameter("month"));
+        String description = request.getParameter("description");
+        int amount = Integer.parseInt(request.getParameter("amount"));
+        try {
+            record = (Record) Class.forName("com.khahani.demo.account." + request.getParameter("status"))
+                    .newInstance();
+
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        if (null != record) {
+            record.setAmount(amount);
+            record.setDay(day);
+            record.setMonth(month);
+            record.setDescription(description);
+        }
     }
 }
--- src/main/java/com/khahani/demo/account/Record.java
+++ src/main/java/com/khahani/demo/account/Record.java
@@ -27,6 +27,30 @@ public abstract class Record{
         time = System.currentTimeMillis();
     }
 
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public static class CantEnterZeroAmountException extends RuntimeException {
         @Override
         public String getMessage() {
